b4e41e6670d68d2a821c285eb3c237ecae927ef7
pmarflee <paul.marfleet@gmail.com>
2021-12-05 14:52:17 +0000

    Fixed lint errors

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,20 @@
 import * as Days from './days/index'
 import { readNumbers } from './util'
 
-export function runAllDays()  {
+export function runAllDays() : void  {
   day1();
 }
 
 function day1() {
-  let numbers = readNumbers('./input/day1.txt');
+  const numbers = readNumbers('./input/day1.txt');
   execute(1, 1, () => Days.Day1.calculatePart1(numbers));
   execute(1, 2, () => Days.Day1.calculatePart2(numbers));
 }
 
 function execute(day: number, part: number, fn: () => number) {
-  let startTime = performance.now(),
-      result = fn(),
-      endTime = performance.now();
+  const startTime = performance.now(),
+        result = fn(),
+        endTime = performance.now();
 
   console.log(`Day ${day} Part ${part}: ${result} (${(endTime - startTime).toFixed(3)}ms)`);
 }
--- src/tests/day1.test.ts
+++ src/tests/day1.test.ts
@@ -1,6 +1,6 @@
 import Day1 from '../days/day1/day1'
 
-let numbers = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+const numbers = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
 
 test('Test Part 1', () => {
     expect(Day1.calculatePart1(numbers)).toBe(7);
